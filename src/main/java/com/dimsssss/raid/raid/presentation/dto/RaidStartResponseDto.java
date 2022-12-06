3fdeff5b6aa36ca70f8a27ae85d228012f7a7687
dimsssss <caruen@hanmail.net>
2022-12-06 13:11:26 +0900

    feat: raid 시작 dto
    Description
    request, response dto 추가

--- src/main/java/com/dimsssss/raid/raid/presentation/dto/RaidStartResponseDto.java
+++ src/main/java/com/dimsssss/raid/raid/presentation/dto/RaidStartResponseDto.java
@@ -1,4 +1,15 @@
 package com.dimsssss.raid.raid.presentation.dto;
 
+import com.dimsssss.raid.raid.domain.RaidRecordEntity;
+import lombok.Getter;
+
+@Getter
 public class RaidStartResponseDto {
+    private Long raidRecordId;
+    private boolean canEnter;
+
+    public RaidStartResponseDto(RaidRecordEntity raidRecordEntity) {
+        this.raidRecordId = raidRecordEntity.getRaidRecordId();
+        this.canEnter = true;
+    }
 }
