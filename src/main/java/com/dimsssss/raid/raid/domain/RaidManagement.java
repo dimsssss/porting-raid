dcfe166ab9571ece0ba31f7bf6cfb26936a033c0
dimsssss <caruen@hanmail.net>
2022-12-09 18:19:13 +0900

    feat: raid 종료 코드 추가, NotCantRaidException -> timeoutException

--- src/main/java/com/dimsssss/raid/raid/domain/RaidManagement.java
+++ src/main/java/com/dimsssss/raid/raid/domain/RaidManagement.java
@@ -22,17 +22,30 @@ public class RaidManagement {
         return current.isAfter(latestStartRaidDateTime.plusMinutes(raidTime));
     }
 
-    public void validateRaidEnter(BossStateEntity bossStateEntity, LocalDateTime current) throws NotCantRaidException {
+    public void validateRaidEnter(BossStateEntity bossStateEntity, LocalDateTime current) throws RaidTimeoutException {
         if (!isTimeOut(current, bossStateEntity)) {
-            throw new NotCantRaidException("현재 레이드가 진행중입니다");
+            throw new RaidTimeoutException("현재 레이드가 진행중입니다");
         }
         if (!isTimeOut(current, bossStateEntity) && isRaiding(bossStateEntity)) {
-            throw new NotCantRaidException("현재 레이드가 진행중입니다");
+            throw new RaidTimeoutException("현재 레이드가 진행중입니다");
+        }
+    }
+
+    public void validateRaidEnd(BossStateEntity bossStateEntity, LocalDateTime current, Long userId) throws RaidTimeoutException {
+        if (isTimeOut(current, bossStateEntity)) {
+            throw new RaidTimeoutException("주어진 레이드 시간이 지났습니다");
+        }
+        if (!bossStateEntity.getLatestRaidUserId().equals(userId)) {
+            throw new IllegalArgumentException("레이드 진행중인 아이디와 종료 아이디가 다릅니다. raidUserId: %s, requestUserId: %s");
         }
     }
 
     public void enterRaid(BossStateEntity bossStateEntity, LocalDateTime current) {
         bossStateEntity.onRaid();
         bossStateEntity.setRaidStartAt(current);
+    }
+
+    public void endRaid(BossStateEntity bossStateEntity) {
+        bossStateEntity.offRaid();
     }
 }
