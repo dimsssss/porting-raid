3e8f1e76914c5d293e7c61970784bd29d7d4ec2d
dimsssss <caruen@hanmail.net>
2022-12-09 18:19:39 +0900

    feat: raid 종료 request dto 추가

---
+++ src/main/java/com/dimsssss/raid/raid/presentation/dto/RaidEndRequestDto.java
@@ -0,0 +1,11 @@
+package com.dimsssss.raid.raid.presentation.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class RaidEndRequestDto {
+    private Long userId;
+    private Long raidRecordId;
+}
