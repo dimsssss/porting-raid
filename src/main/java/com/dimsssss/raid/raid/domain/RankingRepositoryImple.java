18d77ca8f045616cfbfe5048d6ba2ea317f6ea0b
dimsssss <caruen@hanmail.net>
2022-12-13 15:23:16 +0900

    refactor: startRaid 리팩터링
    Description
    service: 불필요한 코드 제거
    repository: 많은 작업을 하던 메서드를 분리함

--- src/main/java/com/dimsssss/raid/raid/domain/RankingRepositoryImple.java
+++ src/main/java/com/dimsssss/raid/raid/domain/RankingRepositoryImple.java
@@ -1,10 +1,14 @@
 package com.dimsssss.raid.raid.domain;
 
+import com.dimsssss.raid.raid.presentation.dto.RankingResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import static java.lang.Long.parseLong;
@@ -14,18 +18,64 @@ import static java.lang.Long.parseLong;
 public class RankingRepositoryImple {
 
     private final StringRedisTemplate stringRedisTemplate;
+    private static final String KEY = "leaderboard";
 
-    public void save(RankingEntity rankingEntity) {
-        String key = "leadreboard:" + rankingEntity.getUserId();
-        stringRedisTemplate.opsForZSet().addIfAbsent(key, rankingEntity.getUserId().toString(), rankingEntity.getTotalScore());
+    private int getPrevScore(Long userId) {
+        Set<ZSetOperations.TypedTuple<String>> tuples = getAllScores();
+
+        for (ZSetOperations.TypedTuple<String> stringTypedTuple: tuples) {
+            if (parseLong(stringTypedTuple.getValue()) == userId) {
+                return stringTypedTuple.getScore().intValue();
+            }
+        }
+        return 0;
     }
 
-    public RankingEntity findById(Long userId) {
-        String key = "leadreboard:" + userId;
-        Set<ZSetOperations.TypedTuple<String>> result = stringRedisTemplate.opsForZSet().distinctRandomMembersWithScore(key, 1);
-        return result.stream().map((ranking) -> RankingEntity.builder()
-                .userId(parseLong(ranking.getValue()))
-                .totalScore(ranking.getScore().intValue())
-                .build()).findFirst().orElse(null);
+    public void save(RaidRecordEntity raidRecordEntity) {
+        int prevScore = getPrevScore(raidRecordEntity.getUserId());
+        int totalScore = prevScore + raidRecordEntity.getScore();
+        String userId = raidRecordEntity.getUserId().toString();
+        stringRedisTemplate.opsForZSet().addIfAbsent(KEY, userId, totalScore);
+    }
+
+    private Set<ZSetOperations.TypedTuple<String>> getAllScores() {
+        return stringRedisTemplate.opsForZSet().reverseRangeWithScores(KEY, 0, -1);
+    }
+
+    private List<RankingEntity> getRankAtAllScores(Set<ZSetOperations.TypedTuple<String>> scores) {
+        int rank = 0;
+        Iterator iterator = scores.iterator();
+        List<RankingEntity> rankers = new ArrayList<>();
+
+        while (iterator.hasNext()) {
+            ZSetOperations.TypedTuple<String> stringTypedTuple = (ZSetOperations.TypedTuple<String>) iterator.next();
+            RankingEntity rankingEntity = RankingEntity.builder()
+                    .ranking(rank)
+                    .userId(parseLong(stringTypedTuple.getValue()))
+                    .totalScore(stringTypedTuple.getScore().intValue())
+                    .build();
+            rankers.add(rankingEntity);
+            rank += 1;
+        }
+        return rankers;
+    }
+
+    private RankingEntity getMyRank(List<RankingEntity> rankers, Long userId) {
+        for (RankingEntity rank: rankers) {
+            if (rank.getUserId() == userId) {
+                return rank;
+            }
+        }
+        return null;
+    }
+
+    public RankingResponseDto getTopTankingAndMyRanking(Long userId) {
+        Set<ZSetOperations.TypedTuple<String>> scores = getAllScores();
+        RankingResponseDto rankingResponseDto = new RankingResponseDto();
+        List<RankingEntity> rankers = getRankAtAllScores(scores);
+        RankingEntity myRank = getMyRank(rankers, userId);
+        rankingResponseDto.setTopRankerInfoList(rankers);
+        rankingResponseDto.setMyRankingInfo(myRank);
+        return rankingResponseDto;
     }
 }
