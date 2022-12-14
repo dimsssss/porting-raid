24005f04eb81eff2309749e172a51f861e2bb865
dimsssss <caruen@hanmail.net>
2022-12-14 22:37:02 +0900

    feat: repository 조회
    Description
    findByUserId 조회

--- src/main/java/com/dimsssss/raid/raid/domain/RaidRecordRepository.java
+++ src/main/java/com/dimsssss/raid/raid/domain/RaidRecordRepository.java
@@ -1,7 +1,12 @@
 package com.dimsssss.raid.raid.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface RaidRecordRepository extends JpaRepository<RaidRecordEntity, Long> {
-
+    @Query(value = "SELECT raid_record_id, score, raid_start_at, raid_end_at, user_id FROM raid_record_entity WHERE user_id = :userId", nativeQuery = true)
+    List<RaidRecordEntity> findByUserId(@Param("userId") Long userId);
 }
