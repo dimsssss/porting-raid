e1bce307bea4c15bdd14728e707a1334ae309b42
dimsssss <caruen@hanmail.net>
2022-12-25 14:11:36 +0900

    feat: 이벤트 발생 시간 추가
    Description
    서버에서 시간을 생성하는 것을 막기 위해 추가

--- src/main/java/com/dimsssss/raid/raid/presentation/dto/RaidEndRequestDto.java
+++ src/main/java/com/dimsssss/raid/raid/presentation/dto/RaidEndRequestDto.java
@@ -3,10 +3,13 @@ package com.dimsssss.raid.raid.presentation.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Builder
 @Getter
 public class RaidEndRequestDto {
     private Long userId;
     private Long raidRecordId;
     private Long bossStateId;
+    private LocalDateTime raidEndTime;
 }
--- src/main/java/com/dimsssss/raid/raid/presentation/dto/RaidStartRequestDto.java
+++ src/main/java/com/dimsssss/raid/raid/presentation/dto/RaidStartRequestDto.java
@@ -1,18 +1,23 @@
 package com.dimsssss.raid.raid.presentation.dto;
 
 import com.dimsssss.raid.raid.domain.RaidRecordEntity;
+
 import lombok.Builder;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @Builder
 @Getter
 public class RaidStartRequestDto {
     private Long userId;
     private int level;
+    private LocalDateTime raidStartTime;
 
-    public RaidStartRequestDto(Long userId, int level) {
+    public RaidStartRequestDto(Long userId, int level, LocalDateTime raidStartTime) {
         this.userId = userId;
         this.level = level;
+        this.raidStartTime = raidStartTime;
     }
     public RaidRecordEntity convertFrom() {
         return RaidRecordEntity.builder()
